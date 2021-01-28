abca06e15b8b7aa7306e9a1529d4b67fd7b39844
Zhao, Huijun <huijunzhao.ds@gmail.com>
2021-01-28 16:10:40 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo “\
-[general]\n\
-email = \”huijunzhao.ds@gmail.com\”\n\
+echo "[general]
+email = \"huijunzhao.ds@gmail.com\"\
 “ > ~/.streamlit/credentials.toml
-echo “\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo “[server]\
+headless = true\
+enableCORS=false\
+port = $PORT\
 “ > ~/.streamlit/config.toml
